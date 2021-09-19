30eebadca6e9a0beefa105e2f137ad34c7b22609
Mohamed Fawzy <mohamed.fawzy.zikrallah@gmail.com>
2021-09-19 21:03:03 +0200

    Redesign Airlines View

---
+++ AirlineApp/Model/Airline.swift
@@ -0,0 +1,15 @@
+//
+//  Airline.swift
+//  AirlineApp
+//
+//  Created by mohamed fawzy on 19September//2021.
+//
+
+import Foundation
+
+struct Airline: Codable {
+    let name: String
+    let country: String
+    let slogan: String
+    let headQuaters: String
+}
